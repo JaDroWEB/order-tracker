1d0f888255224658c31d657cd441a4b7a56b3b25
jadroweb <drobnyjakub357@gmail.com>
2024-07-14 17:39:48 +0200

    Add langugaes logic and UI in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { LanguageService } from './services/language.service';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, MatToolbarModule, MatMenuModule, MatButtonModule, TranslateModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-    constructor(private router: Router) {
-        this.router.events.subscribe((event) => {
-            console.log(event);
-        });
+    private readonly languages = signal<string[]>(this.languageService.getLanguages());
+    public readonly langs = computed(() => this.languages().filter((lang) => lang !== this.currentLang()));
+    public readonly currentLang = signal<string>(this.languageService.getCurrentLanguage());
+    public readonly currentLangUpper = computed(() => this.currentLang().toUpperCase());
+
+    constructor(private readonly languageService: LanguageService) {}
+
+    public setLanguage(lang: string): void {
+        this.currentLang.set(lang);
+        this.languageService.setLanguage(lang);
     }
 }
