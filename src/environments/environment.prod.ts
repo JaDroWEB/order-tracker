4e186e84e04b28e04247ac25b7cc4e1e4cb99ff1
jadroweb <drobnyjakub357@gmail.com>
2024-07-14 17:35:49 +0200

    Add environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://66073047be53febb857f49ae.mockapi.io/api/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://66073047be53febb857f49ae.mockapi.io/api/'
+};
