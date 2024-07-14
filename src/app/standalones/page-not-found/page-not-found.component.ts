1f8af1200a08f4e10fd85aaf2a313b9c56555f86
jadroweb <drobnyjakub357@gmail.com>
2024-07-14 17:37:04 +0200

    Add page not found component

--- src/app/standalones/page-not-found/page-not-found.component.ts
+++ src/app/standalones/page-not-found/page-not-found.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-page-not-found',
     standalone: true,
     templateUrl: './page-not-found.component.html',
     styleUrls: ['./page-not-found.component.scss'],
-    imports: [RouterLink],
+    imports: [RouterLink, TranslateModule],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PageNotFoundComponent {}
